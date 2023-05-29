52721b13ccf76c02f377092a5787b93fe1a62351
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-05-29 19:22:26 +0300

    Clean blog project Section 3 -  HW_9

--- NodeJs/clean-blog/app.js
+++ NodeJs/clean-blog/app.js
@@ -1,19 +1,33 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
+const path = require("path");
+const Post = require("./models/Post.js");
+const port = 3000;
+
 const app = express();
 
-const port = 3000;
+// Connect DB
+
+mongoose.connect("mongodb://127.0.0.1/clean-blog-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set("view engine", "ejs");
 
 //Middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -26,6 +40,11 @@ app.get("/add_post", (req, res) => {
 
 app.get("/post", (req, res) => {
   res.render("post");
+});
+
+app.post("/blogs", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
